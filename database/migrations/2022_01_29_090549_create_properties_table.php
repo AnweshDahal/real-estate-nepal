49787f9d7e6f108a76967835eb8c22d0d8b1d38f
AnweshDahal <anweshd2000@gmail.com>
2022-01-29 15:42:40 +0545

    Make migration to create properties table

---
+++ database/migrations/2022_01_29_090549_create_properties_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('locality_id')->constrained('localities');
+            $table->string('property_name');
+            $table->string('address');
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
