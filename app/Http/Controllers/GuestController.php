1bc9c489a53ce9c480110606e313c5ec3a89aad6
AnweshDahal <anweshd2000@gmail.com>
2022-03-26 22:28:07 +0545

    Add controller for guest user

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Property;
+
+class GuestController extends Controller
+{
+    // Index view for users that have not logged in
+    public function index(){
+        // Latest Properties with thumbnails
+        $properties = Property::latest()->with(['image' => function($query) {
+            $query->where('is_thumbnail', true);
+        }])->get();
+        return view('welcome', compact('properties'));
+    }
+}
