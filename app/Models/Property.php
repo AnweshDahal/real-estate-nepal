07925eda2d3a142eb5cdc9e5d7b5ff57068ef37f
AnweshDahal <anweshd2000@gmail.com>
2022-03-26 22:29:20 +0545

    Add fields to property model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Image;
+use App\Models\Locality;
+use App\Models\User;
 
 class Property extends Model
 {
@@ -22,6 +25,10 @@ class Property extends Model
         'latitude',
         'longitude',
         'description',
+        'listing_type',
+        'property_category',
+        'price',
+        'property_size',
     ];
 
     // Date Attributes
@@ -31,4 +38,32 @@ class Property extends Model
         'deleted_at',
     ];
 
+    // Listing Types
+    const LISTING_TYPES = [
+        'SALE' => 'SALE',
+        'RENT' => 'RENT',
+    ];
+
+    // Property Categories
+    const PROPERTY_CATEGORY = [
+        'LAND' => 'LAND',
+        'HOUSE' => 'HOUSE',
+        'APARTMENT' => 'APARTMENT',
+        'ROOM' => 'ROOM',
+    ];
+
+    public function image()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function locality()
+    {
+        return $this->belongsTo(Locality::class, 'locality_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
