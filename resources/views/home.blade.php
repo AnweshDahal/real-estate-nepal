31ba4b41a0b76221461234c3adafaca60a4978c1
AnweshDahal <anweshd2000@gmail.com>
2022-03-27 11:27:11 +0545

    Update layout for home and welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,24 +5,26 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="search-section">
+        <h1 class="section-title semi bold text-light text-center mb-3">Find a Home that Suits you!</h1>
+        <form action="." method="post">
+            <div class="searchbar">
+                <input type="text" name="search" id="search" class="searchbar-text" placeholder="Select a City">
+                <button class="searchbar-btn text-theme-blue"><i class="bi bi-search"></i></button>
+            </div>
+        </form>
+    </div>
+    <div class="property-index-container">
+        <div class="card w-100 border-0">
+            <div class="card-body">
+                <div class="row">
                     @foreach ($properties as $property)
-                        @include('partials.propertyCard', $property)
+                        <div class="col-12 col-md-12 col-lg-3 mb-3">
+                            @include('partials.propertyCard', $property)
+                        </div>
                     @endforeach
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,26 @@
 @endsection
 
 @section('content')
-    <h1>Real Estate Nepal</h1>
-    @foreach ($properties as $property)
-        @include('partials.propertyCard',  $property )
-    @endforeach
+    <div class="search-section">
+        <h1 class="section-title semi bold text-light text-center mb-3">Find a Home that Suits you!</h1>
+        <form action="." method="post">
+            <div class="searchbar">
+                <input type="text" name="search" id="search" class="searchbar-text" placeholder="Select a City">
+                <button class="searchbar-btn text-theme-blue"><i class="bi bi-search"></i></button>
+            </div>
+        </form>
+    </div>
+    <div class="property-index-container">
+        <div class="card w-100 border-0">
+            <div class="card-body">
+                <div class="row">
+                    @foreach ($properties as $property)
+                        <div class="col-12 col-md-12 col-lg-3 mb-3">
+                            @include('partials.propertyCard', $property)
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
