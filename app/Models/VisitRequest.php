421327ff38cc12c5f7d0cfca79791565e04d9f92
AnweshDahal <anweshd2000@gmail.com>
2022-04-02 18:29:31 +0545

    Add relationsip between VR, property, and user

--- app/Models/VisitRequest.php
+++ app/Models/VisitRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use App\Models\User;
+
 class VisitRequest extends Model
 {
     use HasFactory, SoftDeletes;
@@ -17,7 +19,9 @@ class VisitRequest extends Model
     protected $fillable = [
         'visit_date',
         'requestor',
+        'requestee',
         'property_id',
+        'status',
     ];
 
     // Date attributes
@@ -28,8 +32,30 @@ class VisitRequest extends Model
         'deleted_at',
     ];
 
+    const STATUS = [
+        'PENDING' => 'PENDING',
+        'ACCEPTED' => 'ACCEPTED',
+        'REJECTED' => 'REJECTED',
+    ];
+
     // Attributes that should be cast
     protected $cast = [
         "visit_date" => "datetime",
     ];
+
+    // Getting the property for which teh visit request has been made
+    public function property()
+    {
+        return $this->hasOne(Property::class);
+    }
+
+    public function requestorInfo()
+    {
+        return $this->belongsTo(User::class, 'requestor');
+    }
+
+    public function requesteeInfo()
+    {
+        return $this->belongsTo(User::class, 'requestee');
+    }
 }
